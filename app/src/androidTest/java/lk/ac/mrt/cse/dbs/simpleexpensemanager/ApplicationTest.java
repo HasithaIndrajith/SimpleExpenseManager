a7cf4b3f08f590c1d69dbfa272820ba38651b2d7
HasithaIndrajith <hasitha.19@cse.mrt.ac.lk>
2022-05-11 03:40:34 +0530

    Commit with adding test cases

--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
@@ -16,14 +16,43 @@
 
 package lk.ac.mrt.cse.dbs.simpleexpensemanager;
 
+import static org.junit.Assert.assertTrue;
+
 import android.app.Application;
+import android.content.Context;
 import android.test.ApplicationTestCase;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.ExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.PersistentExpenseManager;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest {
+
+    private static ExpenseManager expenseManager;
+
+    @Before
+    public void setUp() {
+        Context context = ApplicationProvider.getApplicationContext();
+        expenseManager = new PersistentExpenseManager(context);
     }
+
+
+    @Test
+    public void testAddAccount() {
+//        assertTrue(true);
+        expenseManager.addAccount("12","ABC","Hasitha",200);
+        List<String> accountNumbersList = expenseManager.getAccountNumbersList();
+        assertTrue(accountNumbersList.contains("12"));
+    }
+
+
 }
